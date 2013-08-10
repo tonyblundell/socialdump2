ffc687d37a52bb6490ee8074f7c51eb359c8bdbc
Tony Blundell <tony@tonyb82.(none)>
2013-08-10 01:20:33 +0000

    move personalizations to settings

--- socialdump2/main/views.py
+++ socialdump2/main/views.py
@@ -1,9 +1,13 @@
+from django.conf import settings
 from django.shortcuts import render_to_response
 from main.models import Feed
 
 def index(request):
     template = 'main/index.html'
     context = {
-        'feeds': Feed.objects.all()
+        'feeds': Feed.objects.all(),
+        'title': settings.SD2_TITLE,
+        'subtitle': settings.SD2_SUBTITLE,
+        'mail': settings.SD2_MAIL,
     }
     return render_to_response(template, context)
